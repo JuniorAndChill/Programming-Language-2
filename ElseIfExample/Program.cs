83864913500fdfabdac8075161f93324072d4701
Daniel Critchlow <187431975+JuniorAndChill@users.noreply.github.com>
2025-02-06 12:31:49 -0600

    Update comments

--- ElseIfExample/Program.cs
+++ ElseIfExample/Program.cs
@@ -15,11 +15,16 @@ namespace ElseIfExample
                 i.e. 4 is a multiple of 8
                 i.e. 2 is not a multiple of 5*/
             
-            //take the input 
-            Console.Write("Input a number: ");
+            //take the input string and convert to integer
+            //using "Write" instead of "WriteLine" allows for multiple inputs without interuption
+            Console.Write("Input a number to determine multiples against: ");
             int uNumber1 = int.Parse(Console.ReadLine());
-            Console.Write("Input another number: ");
+
+            //take and convert second value
+            Console.Write("Input another number to check if multiple: ");
             int uNumber2 = int.Parse(Console.ReadLine());
+
+            //"if else" statements to determine "if multiple", "if opposite is multiple", and "if not"
             if(uNumber1%uNumber2==0)
             {
                 Console.WriteLine(uNumber1 + " is a multiple of " + uNumber2 + ".");
@@ -33,6 +38,7 @@ namespace ElseIfExample
                 Console.WriteLine("These are not multiples.");
             }
             
+            //Console.RL to finish displaying result
             Console.ReadLine();
         }
     }
