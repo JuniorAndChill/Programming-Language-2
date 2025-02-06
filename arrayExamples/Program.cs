656015c2a9b2a5acfeeaaf111b80e638489026a7
Daniel Critchlow <187431975+JuniorAndChill@users.noreply.github.com>
2025-02-06 12:47:55 -0600

    Update comments

--- arrayExamples/Program.cs
+++ arrayExamples/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*Daniel Critchlow Jr*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,15 @@ namespace ArrayExample
     {
         static void Main(string[] args)
         {
-            int[,]arr2D = new int[3,3];
-            int lengthInFirstDimension = arr2D.GetLength(0);
+            //Basic look at arrays and how to manipulate them
+
+            //declare array and form with [], [,], [,,], etc.
+            //This is for dimension decliration
+            int[,]arr2D = new int[3,3]; //new creates array and [3,3] is the size per dimension
+
+            //using length you can store how many elements are contained in array
+            int lengthInFirstDimension = arr2D.GetLength(0);//0 indicates the dimension to check
+
             Console.WriteLine(lengthInFirstDimension);
         }
     }
