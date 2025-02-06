bb6bedac69d98d5a7e1a33bf43682313c0f48fff
Daniel Critchlow <187431975+JuniorAndChill@users.noreply.github.com>
2025-02-06 15:42:23 -0600

    Initial commit

---
+++ ClassWork6/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Daniel Critchlow Jr
+Create a program that determines how many different ways that 
+I can arrange a certain number of items (r) from a larger 
+collection (n)*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork6
+{
+    internal class Program
+    {
+        static int Factorial(int num)
+        {
+            //i set up the multiplication to 1 for the loop
+            int result = 1;
+            for(int i = 2; i <= num; i++)
+            {
+                //when it iterates multiply the result to the index
+                result *= i;
+            }
+            //just checking if the math is right with a print
+            Console.WriteLine(result);
+            //return the math for later
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            //set vars for math
+            Console.Write("Give me the group size: ");
+            int g = int.Parse(Console.ReadLine());
+
+            Console.Write("Give me the size of each team: ");
+            int r = int.Parse(Console.ReadLine());
+
+            //the 1.0 allows for more accurate math from our function
+            Console.WriteLine(1.0*Factorial(g)/Factorial(g-r));
+        }
+    }
+}
---
+++ MethodExample/Program.cs
@@ -0,0 +1,78 @@
+﻿/*Daniel Critchlow Jr.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MethodExample
+{
+    internal class Program
+    {
+        //methods are functions that are outside of "void Main"
+        //typically to be called multiple times. Makes code readable
+        static void MyMethod()//in c# typically you use Pascal case (c++ is camel)
+        {
+            Console.WriteLine("I just got excecuted");
+        }
+        static void PrintNum(int num)
+        {
+            Console.WriteLine(num);
+        }
+        static void PrintsNumbers(int num1, int num2, int num3)
+        {
+            //Console.WriteLine(num1+num2+num3);
+            int result = num1 + num2 + num3;
+            //here we can see you can call a method into a method and get results
+            Adding(result);
+        }
+        static void Adding(int num)
+        {
+            Console.WriteLine(num+2);
+        }
+        static void MyDef(string country = "Norway")
+        {
+            //This example shows a way you can pass a default value if nothing was called
+            Console.WriteLine(country);
+        }
+        static int AddNum(int num4, int num5)
+        {
+            return num4+num5;
+        }
+        static void Main(string[] args)
+        {
+            //using methods I can manipulate main easier
+            Console.WriteLine("This is a single use of my method");
+            MyMethod();
+
+            Console.WriteLine("\nThis is an example of looped use");
+            for(int i = 0; i < 5; i++)
+            {
+                MyMethod();
+            }
+
+            Console.WriteLine("\nThis is an example passing values");
+
+            PrintNum(5);
+
+            int num1 = 3, num2 = 4, num3 =7;
+            Console.WriteLine("\nThis is an example with multiple values");
+
+            PrintsNumbers(num1,num2,num3);
+
+            Console.WriteLine("\nThis is an example of setting default values (Norway is default)");
+            MyDef("India");
+            MyDef("Sweden");
+            MyDef();
+            MyDef("USA");
+
+            if(AddNum(3,4)==7)
+            {
+                Console.WriteLine("\nThis is correct");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
